aab3f94c0f4379d4ab88c9836427ab59d1eccf7b
eviancha@dichter-neira.com <eviancha@dichter-neira.com>
2026-06-17 10:16:10 -0500

    bloque 15

--- python/python_taller_test.py
+++ python/python_taller_test.py
@@ -1,15 +1,3 @@
-# -----------------------------------------------------
-# 🔴 EJERCICIO 3 — DETECTOR DE DUPLICADOS
-# -----------------------------------------------------
-# ENUNCIADO
-
-# Dada una lista:
-
-numeros = [1,2,2,3,4,4,5,6,6]
-
-# Debes:
-
-# Obtener duplicados
-# Obtener únicos
-# Contar ocurrencias
-# Retornar todo en un diccionario
+numbers = list(map(int, input().strip().split(",")))
+#numbers = list(input().strip().split())
+print(numbers)
